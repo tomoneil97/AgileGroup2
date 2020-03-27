ca20b74c43730b3636d238b3e28051a309060c38
Harve <harvey290@hotmail.co.uk>
2020-03-27 17:46:07 +0000

    BackEnd work
    Added Code to create User Class to SignUp
    
    Added User Class
    
    Added SQL Database (again has locked me out?)

---
+++ CarPoolSite/App_Code/User.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for User
+/// </summary>
+public class User
+{
+    private int id;
+    private string username;
+    private string password;
+    private int imageId;
+
+    private string fName;
+    private string sName;
+    private string gender;
+    private string course;
+
+    private bool isDriver = false;
+
+    public int ID
+    {
+        get { return id; }
+
+        set { id = value; }
+    }
+    public string USERNAME
+    {
+        get { return username; }
+
+        set { username = value; }
+    }
+    public string PASSWORD
+    {
+        get { return password; }
+
+        set { password = value; }
+    }
+    public int IMAGEID
+    {
+        get { return imageId; }
+
+        set { imageId = value; }
+    }
+
+    public string FNAME
+    {
+        get { return fName; }
+
+        set { fName = value; }
+    }
+    public string SNAME
+    {
+        get { return sName; }
+
+        set { sName = value; }
+    }
+    public string GENDER
+    {
+        get { return gender; }
+
+        set { gender = value; }
+    }
+    public string COURSE
+    {
+        get { return course; }
+
+        set { course = value; }
+    }
+
+    public bool ISDRIVER
+    {
+        get { return isDriver; }
+
+        set { isDriver = value; }
+    }
+
+
+    public User(string Username, string Password, string Firstname, string Surname, string Gender)
+    {
+        Random rnd = new Random();
+        ID = rnd.Next(0, 999999);
+        USERNAME = username;
+        PASSWORD = password;
+        FNAME = Firstname;
+        SNAME = Surname;
+        GENDER = Gender;
+
+    }
+
+    
+}
---
+++ CarPoolSite/SignUp.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.IO;
+using System.Data.SqlClient;
+using System.Configuration;
+
+public partial class SignUp :  System.Web.UI.Page
+{
+    public int newUserId;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string userName = Request.Form["uname"];
+        string passWord = Request.Form["psw"];
+        string foreName = Request.Form["forename"];
+        string surName = Request.Form["surname"];
+        string gender = Request.Form["gender"];
+
+        User newUser = new User(userName, passWord, foreName, surName, gender);
+        newUserId = newUser.ID;
+    }
+
+    protected void btnUpload_Click(object sender, EventArgs e)
+    {
+        
+    }
+}
