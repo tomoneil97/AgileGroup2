dbad5edcfc4082f145cda5ef826b4dbde0791c9b
tomoneil97 <58866675+tomoneil97@users.noreply.github.com>
2020-05-03 18:48:57 +0100

    Attempts at testing

---
+++ unitTest_Project2/UnitTest2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace unitTest_Project2
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            User myUser = new User();   
+        }
+    }
+}
